eb9c96411ff0dbc9275b38bc09d5d58a8e576165
norie <norieli@qq.com>
2021-09-22 16:37:50 +0800

    用户管理

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,5 +12,6 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
-
+    // $router->get('users', 'UsersController@index');
+    $router->resource('users', UserController::class);
 });
