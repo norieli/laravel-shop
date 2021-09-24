6249740ef663990aaef5efc0943d864edc82a513
norie <norieli@qq.com>
2021-09-24 17:20:54 +0800

    商品评价

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,9 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
 
         Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+
+        Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
+        Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
         
     });
     // 结束
